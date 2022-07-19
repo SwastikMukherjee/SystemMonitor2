e94da230f2361f7b1d68886bf24b4611a0d99c55
Swastik Mukherjee <51983729+SwastikMukherjee@users.noreply.github.com>
2022-07-20 00:19:13 +0530

    Update linux_parser.h

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -17,7 +17,7 @@ const std::string kMeminfoFilename{"/meminfo"};
 const std::string kVersionFilename{"/version"};
 const std::string kOSPath{"/etc/os-release"};
 const std::string kPasswordPath{"/etc/passwd"};
-
+ 
 // System
 float MemoryUtilization();
 long UpTime();
@@ -30,16 +30,17 @@ std::string Kernel();
 // CPU
 enum CPUStates {
   kUser_ = 0,
-  kNice_,
-  kSystem_,
-  kIdle_,
-  kIOwait_,
-  kIRQ_,
-  kSoftIRQ_,
-  kSteal_,
-  kGuest_,
-  kGuestNice_
+  kNice_ = 1,
+  kSystem_ = 2,
+  kIdle_ = 3,
+  kIOwait_ = 4,
+  kIRQ_ = 5,
+  kSoftIRQ_ = 6,
+  kSteal_ = 7,
+  kGuest_ = 8,
+  kGuestNice_ = 9
 };
+
 std::vector<std::string> CpuUtilization();
 long Jiffies();
 long ActiveJiffies();
@@ -47,6 +48,7 @@ long ActiveJiffies(int pid);
 long IdleJiffies();
 
 // Processes
+float ProcessCpuUtilization(int pid);
 std::string Command(int pid);
 std::string Ram(int pid);
 std::string Uid(int pid);
