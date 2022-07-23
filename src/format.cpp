9d67aabfa1135074742569a8724782b0f0d4015e
Swastik Mukherjee <51983729+SwastikMukherjee@users.noreply.github.com>
2022-07-23 23:36:50 +0530

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -3,37 +3,35 @@
 
 using std::string;
 
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
 std::string Format::ElapsedTime(long seconds) { 
     long hr, min, sec;
     hr = seconds/3600;
     min = (seconds - 3600*hr)/60;
     sec = (seconds - 3600*hr)%60;
 
-    std::string HH, MM, SS;
+    std::string H, M, S;
     if (hr > 99) {
-        HH = "-99";
+        H = "-99";
     } else if (hr >= 10 && hr <= 99 ){
-        HH = std::to_string(hr);
+        H = std::to_string(hr);
     } else {
-        HH = "0" + std::to_string(hr);
+        H = "0" + std::to_string(hr);
     }
 
     if (min >= 10 && min < 60) {
-        MM = std::to_string(min);
+        M = std::to_string(min);
     } else if (min < 10) {
-        MM = "0" + std::to_string(min);
+        M = "0" + std::to_string(min);
     } else {
-        MM = "-99";
+        M = "-99";
     }
 
     if (sec >= 10 && sec < 60) {
-        SS = std::to_string(sec);
+        S = std::to_string(sec);
     } else if (sec < 10) {
-        SS = "0" + std::to_string(sec);
+        S = "0" + std::to_string(sec);
     } else {
-        SS = "-99";
+        S = "-99";
     }
-    return HH + ":" + MM + ":" + SS; 
+    return H + ":" + M + ":" + S; 
 }
